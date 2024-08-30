acb6aa295d04c58e493d9709033b1b95442e6f81
admamr <71948519+admamr@users.noreply.github.com>
2024-08-30 16:21:19 +0300

    update

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  });
+});
+
 let nextButton = document.getElementById("next");
 let prevButton = document.getElementById("prev");
 let carousel = document.querySelector(".carousel");
